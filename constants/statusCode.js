ea5b2682895dfb9a0d145917987e9e84fb3387e5
incmrijdty <alesia.sichova@yandex.by>
2025-03-25 18:51:45 +0100

    Update statusCode.js

--- constants/statusCode.js
+++ constants/statusCode.js
@@ -1,8 +1,11 @@
 // 🔧 Zdefiniuj obiekt STATUS_CODE, który przechowuje kody statusu HTTP 302 (FOUND) oraz 404 (NOT_FOUND).
 const STATUS_CODE = {
     FOUND: 302,
-    NOT_FOUND: 404
+    NOT_FOUND: 404,
+    BAD_REQUEST: 400,           
+    INTERNAL_SERVER_ERROR: 500  
 };
+
 // 🔧 Wyeksportuj STATUS_CODE, który przechowuje kody statusu HTTP.
 module.exports = { STATUS_CODE };
 
