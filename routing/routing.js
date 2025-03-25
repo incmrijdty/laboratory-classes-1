f41140e16ba12d5192eff489c76c5b844f3059ae
incmrijdty <alesia.sichova@yandex.by>
2025-03-25 18:35:46 +0100

    Update routing.js

--- routing/routing.js
+++ routing/routing.js
@@ -1,5 +1,9 @@
 // 📦 Zaimportuj moduł odpowiedzialne za routing poszczególnych części aplikacji.
 // 📦 Zaimportuj obiekt STATUS_CODE.
+const { homeRouting } = require('./home');
+const { productRouting } = require('./product');
+const { logoutRouting } = require('./logout');
+const { STATUS_CODE } = require('../statusCode');
 
 // 🏗 Stwórz tutaj funkcję 'requestRouting', która będzie obsługiwać zapytania HTTP.
 // Podpowiedź: const requestRouting = (request, response) => {
@@ -12,5 +16,32 @@
 // 🏗 Tutaj stwórz obsługę przypadku, jeśli żądany URL nie istnieje. Zwróć wtedy błąd 404.
 // 🏗 Stwórz również logowanie do konsoli informacji, mówiące o typie logowania (ERROR), dacie oraz informację, że żądany url nie istnieje.
 //  };
+const requestRouting = (request, response) => {
+    const { method, url } = request;
+    const date = new Date().toISOString();
+    
+    console.log(`INFO [${date}]: ${method} - ${url}`);
 
+    if (url === "/") {
+        return homeRouting(method, response);
+    } 
+    
+    if (url.startsWith("/product")) {
+        return productRouting(method, url, response);
+    } 
+    
+    if (url === "/logout") {
+        return logoutRouting(method, response);
+    } 
+
+    if (url === "/kill") {
+        console.log(`PROCESS [${date}]: Logout has been initiated and the application will be closed.`);
+        process.exit();
+    }
+
+    console.log(`ERROR [${date}]: Requested URL ${url} doesn’t exist.`);
+    response.writeHead(STATUS_CODE.NOT_FOUND, { "Content-Type": "text/html" });
+    return response.end("<h1>404 - Page Not Found</h1>");
+};
 // 🔧 Wyeksportuj funkcję 'requestRouting', aby inne moduł mogły jej używać.
+module.exports = { requestRouting };
